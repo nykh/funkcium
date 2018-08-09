0a23b5869492cf1edd91a79caa06ddc7136528f6
nykh <nicholas.ykhuang@gmail.com>
2018-08-09 01:18:36 -0700

    Add box

--- functor/functor.py
+++ functor/functor.py
@@ -10,3 +10,30 @@ class Functor(Generic[T]):
 
     def flatmap(self, f: Callable[[T], Sequence[U]]) -> 'Functor[U]':
         raise NotImplementedError()
+
+
+class Box(Functor):
+    def __init__(self, v):
+        self.v = v
+
+    def map(self, f):
+        return Box(f(self.v))
+
+    def flatmap(self, f):
+        u = f(v)
+        if isinstance(u, Box):
+            return u
+        else:
+            return Box(u)
+
+    @property
+    def get(self):
+        return self.v
+
+############################## test ################################
+
+
+def test_unit_functor_works_as_expected():
+    x = Box(1)
+    y = x.map(lambda x: x + 1).map(lambda x: x * 3).map(lambda x: x - 3)
+    assert y.get == (1 + 1) * 3 - 3
