863e2534e1d3b11f4f36b77061a56cd9b4e89d4c
nykh <nhuang@sumologic.com>
2018-08-10 15:35:14 -0700

    Set up package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funkcium',
+      version='0.1',
+      description='Yet Another Python functional library, with scala inspiration',
+      url='https://github.com/nykh/funkcium',
+      author='nykh',
+      author_email='nicholas.ykhuang@gmail.com',
+      license='MIT',
+      packages=['funkcium'],
+      zip_safe=False)
