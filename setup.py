a73a8032db7e4a7908bb2070bdc5b76a2600b75a
nykh <nhuang@sumologic.com>
2018-08-13 11:52:51 -0700

    Add long description; bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='funkcium',
-      version='0.1',
+      version='0.1.1',
       description='Yet Another Python functional library, with scala inspiration',
-      long_description=readme,
-      long_description_content_type='text/markdown; charset=UTF-8',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/nykh/funkcium',
       author='nykh',
       author_email='nykh.pypi.199421@tryninja.io',
       license='MIT',
       packages=['funkcium'],
-      zip_safe=False,
       classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
