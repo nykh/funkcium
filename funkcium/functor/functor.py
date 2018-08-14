273f2a187902efa2dcb8d3268b9ba82528c7f04d
nykh <nhuang@sumologic.com>
2018-08-14 15:10:49 -0700

    Add pipe (>>) for Box; bump to 0.1.5

--- funkcium/functor/functor.py
+++ funkcium/functor/functor.py
@@ -21,6 +21,9 @@ class Box(Functor):
     def map(self, f):
         return Box(f(self.v))
 
+    def __rshift__(self, f: Callable[[T], U]) -> 'Functor[U]':
+        return self.map(f)
+
     def flatmap(self, f):
         u = f(v)
         if isinstance(u, Box):
@@ -39,3 +42,5 @@ def test_unit_functor_works_as_expected():
     x = Box(1)
     y = x.map(lambda x: x + 1).map(lambda x: x * 3).map(lambda x: x - 3)
     assert y.get == (1 + 1) * 3 - 3
+    z = x >> (lambda x: x + 1) >> (lambda x: x * 3) >> (lambda x: x - 3)
+    assert z.get == (1 + 1) * 3 - 3
